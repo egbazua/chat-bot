b1265f44f6c53aae357dd6c1186c2dfb45d3e51f
Enrique Bazua <enriquegbazua@gmail.com>
2022-03-16 21:43:47 -0700

    window settings

--- main.py
+++ main.py
@@ -3,7 +3,13 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 import tkinter
 
+# Window
 window = tkinter.Tk()
+window.geometry("400x300")
+
+title = tkinter.Label(window, text = "Hola mundo")
+title.pack()
+
 window.mainloop()
 
 chat = ChatBot('Venado')
