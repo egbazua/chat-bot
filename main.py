b4082029abba479783c784fa4157c915b66a0e2f
Enrique Bazua <enriquegbazua@gmail.com>
2022-03-16 20:25:06 -0700

    words settings

--- main.py
+++ main.py
@@ -1,10 +1,13 @@
 # Modules and imports
 from chatterbot import ChatBot
-from chatterbot.trainers import ChatterBotCorpusTrainer
+from chatterbot.trainers import ListTrainer
 
-chat = ChatBot('cctmx')
-trainer = ChatterBotCorpusTrainer(chat)
-trainer.train("chatterbot.corpus.spanish.greetings")
+chat = ChatBot('Venado')
+talk = ['Hola', '¿Qué tal?', 'Tengo una pregunta', 'Si, dime',
+        'La comida en méxico es buena?', 'Si, es deliciosa?', 
+        '¿Qué me recomiendas?', 'Tamales', 'Muchas gracias']
+trainer = ListTrainer(chat)
+trainer.train(talk)
 
 while True:
     request = input('User: ')
