2202ae8b783af81899be00e26689f1699809ade3
Enrique Bazua <enriquegbazua@gmail.com>
2022-03-16 22:42:53 -0700

    defining main function

--- main.py
+++ main.py
@@ -5,42 +5,48 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ListTrainer
 from tkinter import *
 
-# Window
-window = Tk()
-window.geometry("400x300+800+400")
-window.iconbitmap("images/bot.ico")
-window.title("Chat Bot")
-window.resizable(width=False, height=False)
+def main():
+    # ChatBot
+    chat = ChatBot('Venado')
+    talk = ['Hola', '¿Qué tal?', 'Tengo una pregunta', 'Si, dime',
+            'La comida en méxico es buena?', 'Si, es deliciosa?', 
+            '¿Qué me recomiendas?', 'Tamales', 'Muchas gracias']
+    trainer = ListTrainer(chat)
+    trainer.train(talk)
 
-# Title
-title = Label(window, text = "Platica conmigo", font = ('Times 18'))
-title.pack()
+    # Window
+    window = Tk()
+    window.geometry("400x300+800+400")
+    window.iconbitmap("images/bot.ico")
+    window.title("Chat Bot")
+    window.resizable(width=False, height=False)
 
-# Frame
-frame = Frame(window, width=320, height=215, highlightbackground="black", highlightthickness=1)
-frame.pack()
-frame.config(bg="white")
+    # Title
+    title = Label(window, text = "Platica conmigo", font = ('Times 18'))
+    title.pack()
 
-# Footer
-""" footer = tkinter.Label(window, text = "Enrique Bazúa")
-footer.pack(side = tkinter.BOTTOM) """
+    # Frame
+    frame = Frame(window, width=320, height=215, highlightbackground="black", highlightthickness=1)
+    frame.pack()
+    frame.config(bg="white")
 
-# Buttom
-button = ttk.Button(text = "Enviar mensaje")
-button.pack(side = BOTTOM)
+    # Footer
+    """ footer = tkinter.Label(window, text = "Enrique Bazúa")
+    footer.pack(side = tkinter.BOTTOM) """
 
-# TextBox
-textbox = ttk.Entry()
-textbox.pack(side = BOTTOM)
+    # Buttom
+    button = ttk.Button(text = "Enviar mensaje")
+    button.pack(side = BOTTOM)
 
-window.mainloop()
+    # TextBox
+    textbox = ttk.Entry()
+    textbox.pack(side = BOTTOM)
 
-chat = ChatBot('Venado')
-talk = ['Hola', '¿Qué tal?', 'Tengo una pregunta', 'Si, dime',
-        'La comida en méxico es buena?', 'Si, es deliciosa?', 
-        '¿Qué me recomiendas?', 'Tamales', 'Muchas gracias']
-trainer = ListTrainer(chat)
-trainer.train(talk)
+    window.mainloop()
+
+
+# Main Function
+main()
 
 """ while True:
     request = input('User: ')
