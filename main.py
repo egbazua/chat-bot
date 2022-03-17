4399e56b2aea2db64997696d3fdc1b50ec21b752
Enrique Bazua <enriquegbazua@gmail.com>
2022-03-16 19:34:33 -0700

    spanish language

--- main.py
+++ main.py
@@ -3,6 +3,9 @@ from chatterbot import ChatBot
 from chatterbot.trainers import ChatterBotCorpusTrainer
 
 chat = ChatBot('cctmx')
+trainer = ChatterBotCorpusTrainer(chat)
+trainer.train("chatterbot.corpus.spanish.greetings")
+
 while True:
     request = input('User: ')
     answer = chat.get_response(request)
